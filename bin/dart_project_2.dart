6cf37f42b6713cd9efe08357872fc0dff8ba6316
christian <christianwitanto_2428240034@mhs.mdp.ac.id>
2026-09-21 17:58:54 +0700

    update dart project

--- bin/dart_project_2.dart
+++ bin/dart_project_2.dart
@@ -1,5 +1,47 @@
-import 'package:dart_project_2/dart_project_2.dart' as dart_project_2;
-
 void main(List<String> arguments) {
-  print('Hello world: ${dart_project_2.calculate()}!');
+  // pertama
+  var x;//dynamic
+
+  x="hello";
+  print(x);
+  x=123;
+  print(x);
+  x=true;
+  print(x);
+
+  var y ="hello world"; //inisialisasi
+  print(y);
+  //y = 200;
+  
+  //ketiga
+  dynamic z = 100;
+  z=true;
+  print(z);
+  z=100;
+  print(z);
+  z=3.14;
+  print(z);
+
+  //dart => null safety
+  //String nama1;
+  //error karena null
+  //nama1=null
+
+  String? nama2;
+  nama2=null;
+  print(nama2);
+  nama2 ="Budi";
+  print(nama2);
+
+  List<String> dataBuah;
+  dataBuah =["Mangga","Pepaya","Jeruk"];
+  print(dataBuah[0]);
+
+  dataBuah.forEach((element){
+    print(element);
+  });
+
+  for(int i=0;i<dataBuah.length;i++){
+    print(dataBuah[i]);
+  }
 }
